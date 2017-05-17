cb6acd57f30136c1e78cc522fa99887f0d506e36
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-05-16 20:05:22 -0400

    added project place holders

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
   $('body').css('display', 'none');
-  $('body').fadeIn(1000);
+  $('body').fadeIn(3000);
   $('.navbar').click(function(event){
     event.preventDefault();
     newLocation = this.href;
-  $('body').fadeIn(1000, newpage);
+  $('body').fadeIn(3000, newpage);
   });
 
-
   function newpage(){
-    window.location= newLocation;
+    window.location.href = newLocation;
   }
 
 });
